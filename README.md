70b56415db05c846aeb0ad11a358e07fca172829
PatrickSachs <kontakt@patrick-sachs.de>
2021-02-06 23:52:32 +0100

    Updated documentation

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
 # Sahnee.Config
 
-**TODO: Add description**
+Small helper library used inject a commonly used configuration pattern in projects at [Sahnee](https://sahnee.dev).
+
+Our configurations are typically isolated by module, meaning that config values relevant for module `MyModule`
+are set like:
+
+```elixir
+config :your_app, MyModule,
+  value1: true,
+  value2: "hello world"
+```
+
+This library allows us to seamlessly use these values in `MyModule`
+
+```elixir
+defmodule MyModule
+  using Sahnee.Config, keys: [:value1, :value2]
+
+  # Generates:
+  defp env(key), do: Sahnee.Config.env(:your_app, key)
+  defp env_value1(), do: env(:value1)
+  defp env_value2(), do: env(:value2)
+end
+```
+
+Allowing us to simply call the `env_value1/0` function whenever we need to access this value without having to repeat
+the small, but quickly annoying boilerplate code.
+
+## Limitations
+
+By design this library only allows to resolve runtime configuration values by using `Application.fetch_env`.
 
 ## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `sahnee_config` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -15,7 +41,5 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/sahnee_config](https://hexdocs.pm/sahnee_config).
+Documentation can be be found at [https://hexdocs.pm/sahnee_config](https://hexdocs.pm/sahnee_config).
 
--- lib/sahnee/config.ex
+++ lib/sahnee/config.ex
@@ -1,6 +1,35 @@
 defmodule Sahnee.Config do
   default_otp = Application.compile_env(:sahnee_config, :default_otp)
 
+  @moduledoc """
+  Automatically creates an `env/1` function that allows to get a namespaced config value.
+  All configuration values are resolved at runtime only.
+
+  Options:
+
+  - namespace: The namespace of the config values. Defaults to the calling module. Set to `nil` to disable namespacing
+    and use global configuration values.
+  - keys: A list of compile time atoms for which `env_<name>/0` functions will be generated to get the given config
+    value. Serves as a way to statically declare all used values.
+  - otp: The OTP app to get the values from. If not set the `:sahnee_config` `:default_otp` compile time config value is
+    used.
+
+  Example
+
+      defmodule MyModule
+        using Sahnee.Config, keys: [:value1, :value2]
+
+        # Generates:
+
+        defp env(key), do: Sahnee.Config.env(:your_app, key)
+        defp env_value1(), do: env(:value1)
+        defp env_value2(), do: env(:value2)
+      end
+  """
+
+  @doc """
+  Gets a runtime configuration value.
+  """
   @compile {:inline, env: 2}
   @compile {:inline, env: 3}
   def env(otp, key, default \\ nil) do
@@ -10,15 +39,6 @@ defmodule Sahnee.Config do
     end
   end
 
-  @moduledoc """
-  Automatically creates an `env/1` function that allows to get a namespaced config value.
-
-  Options:
-  - namespace: The namespace of the config values. Defaults to the calling module. Set to `nil` to disable namespacing
-    and use global configuration values.
-  - keys: A list of compile time atoms for which `env_<name>/0` functions will be generated to get the given config
-    value. Serves as a way to statically declare all used values.
-  """
   defmacro __using__(opts \\ []) do
     keys = Access.get(opts, :keys, [])
     otp = Access.get(opts, :otp, unquote(default_otp))
